db418c5fe1101afeb6defd73dfd27ef45317e104
Karl Broman <kbroman@gmail.com>
2019-02-18 01:00:07 -0500

    Revise install.R to have just one call to install.packages()

--- install.R
+++ install.R
@@ -1,6 +1,3 @@
-install.packages("broman")
-install.packages("knitr")
-install.packages("rmarkdown")
-install.packages("caTools")
-install.packages("bitops")
-install.packages("rprojroot")
+install.packages(c("broman",
+                   "knitr", "rmarkdown",
+                   "caTools", "bitops", "rprojroot"))
